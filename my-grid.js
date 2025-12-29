c5215e9e666de91e3af17aee7d57d1ce573fadca
dirkarnez <smalldirkalex@gmail.com>
2025-12-29 20:56:19 +0800

    - working wih Higher-Order Component

--- my-grid.js
+++ my-grid.js
@@ -8,41 +8,54 @@ browser version of babel do all the work for us.
 var ReactGridLayoutLibrary = null;
 
 function MyGrid() {
-    // const { width, containerRef, mounted } = ReactGridLayoutLibrary.ReactGridLayout.useContainerWidth();
+  const [ loaded, setLoaded ] = React.useState(false);
 
-    const [ loaded, setLoaded ] = React.useState(false);
+  React.useEffect(() => {
+    fetch("./ReactGridLayoutLibrary.js")
+    .then(a => a.text())
+    .then(a => {
+        eval(a);
+        ReactGridLayoutLibrary.ReactGridLayout.WidthProvider(ReactGridLayoutLibrary.ReactGridLayout);
+        setLoaded(true);
+    });
 
-    React.useEffect(() => {
-        fetch("./ReactGridLayoutLibrary.js")
-        .then(a => a.text())
-        .then(a => {
-            eval(a);
-            ReactGridLayoutLibrary.ReactGridLayout.WidthProvider(ReactGridLayoutLibrary.ReactGridLayout);
-            setLoaded(true);
-        });
+  }, []);
+	return (
+    loaded && <MyGridReal/>
+	);
+}
 
-    }, []);
 
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 }
-  ];
+function MyGridReal() {
+  const { width, containerRef, mounted } = ReactGridLayoutLibrary.ReactGridLayout.useContainerWidth();
 
   return (
-    <div>
-        { loaded &&
+    <div ref={containerRef} style={{width: "100%"}}>
+      {
+        mounted && 
         <ReactGridLayoutLibrary.ReactGridLayout
-          layout={layout}
-          width="500"
-          gridConfig={{ cols: 12, rowHeight: 30 }}
-          dragConfig={{ enabled: true,/* handle: '.handle'*/ }}
-        >
-          <div key="a" style={{borderColor: "#000 !important", borderStyle: "solid"}}>a</div>
-          <div key="b" style={{borderColor: "#000 !important", borderStyle: "solid"}}>b</div>
-          <div key="c" style={{borderColor: "#000 !important", borderStyle: "solid"}}>c</div>
+          width={width}
+          gridConfig={{ cols: 12, rowHeight: 30 }}>
+            <div key="a" style={{borderColor: "#000 !important", borderStyle: "solid"}} data-grid={{ x: 0, y: 0, w: 1, h: 2, static: true }}>
+              a
+            </div>
+            <div key="b" style={{borderColor: "#000 !important", borderStyle: "solid"}} data-grid={{ x: 1, y: 0, w: 3, h: 2 }}>
+              b
+            </div>
+            <div key="c" style={{borderColor: "#000 !important", borderStyle: "solid"}} data-grid={{ x: 4, y: 0, w: 1, h: 2 }}>
+              c
+            </div>
+            <div key="d" style={{borderColor: "#000 !important", borderStyle: "solid"}} data-grid={{ x: 5, y: 0, w: 1, h: 2 }}>
+              c
+            </div>
+            <div key="e" style={{borderColor: "#000 !important", borderStyle: "solid"}} data-grid={{ x: 6, y: 0, w: 1, h: 2 }}>
+              c
+            </div>
+            <div key="f" style={{borderColor: "#000 !important", borderStyle: "solid"}} data-grid={{ x: 7, y: 0, w: 5, h: 2 }}>
+              cds
+            </div>
         </ReactGridLayoutLibrary.ReactGridLayout>
-        }
+      }
     </div>
   );
 }
